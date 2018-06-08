9d3d86bec4a7dc6cdae841b74b3ae1f504c050e7
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-08 15:22:48 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -11,12 +11,9 @@ class RepositoriesController < ApplicationController
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    redirect_to '/'
-    conn = Faraday.new(:url => "https://api.github.com/user/repos")
-    conn.post do |req|
-      req.headers = {"Authorization" => "token #{session[:token]}"}
-      req.body = {"name": JSON.generate(params[:name])}
+    Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers = {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+      req.body = {name: params[:name]}.to_json
     end
     redirect_to root_path
   end
