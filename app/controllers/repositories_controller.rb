bf355b5b42bbd908d883d6f80d21e025e28bbecb
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-08 13:47:52 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -15,6 +15,7 @@ class RepositoriesController < ApplicationController
     Faraday.post("https://api.github.com/user/repos") do |req|
       req.headers = {"Authorization" => "token #{session[:token]}", 'Content-Type' => 'application/json'}
       req.body = "{\"name\": \"a-new-repo\"}"
+      binding.pry
     end
     redirect_to root_path
   end
